5fedf5e39c12dba0218a5934a86b6f256a11bac9
MrHammadSandhu <mrhammadsandhu0@gmail.com>
2024-08-21 16:34:18 +0500

    changing services section

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,18 +1,18 @@
-(function($) {
+(function ($) {
 
 	'use strict';
 	// Mean Menu
 	$('.mean-menu').meanmenu({
 		meanScreenWidth: "991"
 	});
-	
+
 	// Header Sticky, Go To Top JS
-	$(window).on('scroll', function() {
+	$(window).on('scroll', function () {
 		// Header Sticky JS
-		if ($(this).scrollTop() >150){  
+		if ($(this).scrollTop() > 150) {
 			$('.navbar-area').addClass("is-sticky");
 		}
-		else{
+		else {
 			$('.navbar-area').removeClass("is-sticky");
 		};
 
@@ -53,38 +53,6 @@
 		},
 	});
 
-	// Case Slider JS
-	$('.case-slider').owlCarousel({
-		loop: true,
-		margin: 30,
-		nav: true,
-		dots: false,
-		autoplay: false,
-		smartSpeed: 1000,
-		autoplayHoverPause: true,
-		center: true,
-		navText: [
-			"<i class='ri-arrow-left-s-line'></i>",
-			"<i class='ri-arrow-right-s-line'></i>",
-		],
-		responsive: {
-			0: {
-				items: 1,
-			},
-			576: {
-				items: 1,
-			},
-			768: {
-				items: 2,
-			},
-			992: {
-				items: 3,
-			},
-			1200: {
-				items: 2,
-			},
-		},
-	});
 
 	// Testimonials Slider JS
 	$('.testimonials-slider').owlCarousel({
@@ -134,34 +102,22 @@
 			"<i class='ri-arrow-right-s-line'></i>",
 		],
 	});
-	
-	// Click Event JS
-	$('.go-top').on('click', function() {
-		$("html, body").animate({ scrollTop: "0" }, 50);
-	});
 
-	// FAQ Accordion JS
-	$('.accordion').find('.accordion-title').on('click', function(){
-		// Adds Active Class
-		$(this).toggleClass('active');
-		// Expand or Collapse This Panel
-		$(this).next().slideToggle('fast');
-		// Hide The Other Panels
-		$('.accordion-content').not($(this).next()).slideUp('fast');
-		// Removes Active Class From Other Titles
-		$('.accordion-title').not($(this)).removeClass('active');		
+	// Click Event JS
+	$('.go-top').on('click', function () {
+		$("html, body").animate({ scrollTop: "0" }, 50);
 	});
 
 	// Count Time JS
 	function makeTimer() {
-		var endTime = new Date("november  30, 2026 17:00:00 PDT");			
+		var endTime = new Date("november  30, 2026 17:00:00 PDT");
 		var endTime = (Date.parse(endTime)) / 1000;
 		var now = new Date();
 		var now = (Date.parse(now) / 1000);
 		var timeLeft = endTime - now;
-		var days = Math.floor(timeLeft / 86400); 
+		var days = Math.floor(timeLeft / 86400);
 		var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-		var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+		var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600)) / 60);
 		var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
 		if (hours < "10") { hours = "0" + hours; }
 		if (minutes < "10") { minutes = "0" + minutes; }
@@ -171,17 +127,17 @@
 		$("#minutes").html(minutes + "<span>Minutes</span>");
 		$("#seconds").html(seconds + "<span>Seconds</span>");
 	}
-	setInterval(function() { makeTimer(); }, 300);
+	setInterval(function () { makeTimer(); }, 300);
 
 	// Preloader
-	$(window).on('load', function() {
+	$(window).on('load', function () {
 		$('.preloader').addClass('preloader-deactivate');
-	}) 
+	})
 
 	// Subscribe form JS
 	$(".newsletter-form").validator().on("submit", function (event) {
 		if (event.isDefaultPrevented()) {
-		// handle the invalid form...
+			// handle the invalid form...
 			formErrorSub();
 			submitMSGSub(false, "Please enter your email correctly.");
 		} else {
@@ -189,7 +145,7 @@
 			event.preventDefault();
 		}
 	});
-	function callbackFunction (resp) {
+	function callbackFunction(resp) {
 		if (resp.result === "success") {
 			formSuccessSub();
 		}
@@ -197,36 +153,31 @@
 			formErrorSub();
 		}
 	}
-	function formSuccessSub(){
+	function formSuccessSub() {
 		$(".newsletter-form")[0].reset();
 		submitMSGSub(true, "Thank you for subscribing!");
-		setTimeout(function() {
+		setTimeout(function () {
 			$("#validator-newsletter").addClass('hide');
 		}, 4000)
 	}
-	function formErrorSub(){
+	function formErrorSub() {
 		$(".newsletter-form").addClass("animated shake");
-		setTimeout(function() {
+		setTimeout(function () {
 			$(".newsletter-form").removeClass("animated shake");
 		}, 1000)
 	}
-	function submitMSGSub(valid, msg){
-		if(valid){
+	function submitMSGSub(valid, msg) {
+		if (valid) {
 			var msgClasses = "validation-success";
 		} else {
 			var msgClasses = "validation-danger";
 		}
 		$("#validator-newsletter, #validator-newsletter-2").removeClass().addClass(msgClasses).text(msg);
 	}
-	
-	// AJAX MailChimp JS
-	$(".newsletter-form").ajaxChimp({
-		url: "https://Envy Theme.us20.list-manage.com/subscribe/post?u=60e1ffe2e8a68ce1204cd39a5&amp;id=42d6d188d9", // Your url MailChimp
-		callback: callbackFunction
-	});
+
 
 	// ScrollCue JS
-    scrollCue.init();
+	scrollCue.init();
 
 	// Popup Video JS
 	$('.popup-youtube, .popup-vimeo').magnificPopup({
@@ -239,48 +190,20 @@
 	});
 
 	// Odometer JS
-	$('.odometer').appear(function(e) {
+	$('.odometer').appear(function (e) {
 		var odo = $(".odometer");
-		odo.each(function() {
+		odo.each(function () {
 			var countNumber = $(this).attr("data-count");
 			$(this).html(countNumber);
 		});
 	});
 
-	// Search Popup JS
-	$('.close-btn').on('click', function() {
-		$('.search-overlay').fadeOut();
-		$('.search-btn').show();
-		$('.close-btn').removeClass('active');
-	});
-	$('.search-btn').on('click', function() {
-		$(this).hide();
-		$('.search-overlay').fadeIn();
-		$('.close-btn').addClass('active');
-	});
-
-	//skill
-	jQuery('.skill-bar').each(function() {
-		jQuery(this).find('.progress-content').animate({
-		width:jQuery(this).attr('data-percentage')
-		},2000);
-		
-		jQuery(this).find('.progress-number-mark').animate(
-		{left:jQuery(this).attr('data-percentage')},
-		{
-			duration: 2000,
-			step: function(now, fx) {
-			var data = Math.round(now);
-			jQuery(this).find('.percent').html(data + '%');
-			}
-		});  
-	});
 
 	// Tabs 
 	$('.tab ul.tabs').addClass('active').find('> li:eq(0)').addClass('current');
 	$('.tab ul.tabs li').on('click', function (g) {
-		var tab = $(this).closest('.tab'), 
-		index = $(this).closest('li').index();
+		var tab = $(this).closest('.tab'),
+			index = $(this).closest('li').index();
 		tab.find('ul.tabs > li').removeClass('current');
 		$(this).closest('li').addClass('current');
 		tab.find('.tab_content').find('div.tabs_item').not('div.tabs_item:eq(' + index + ')').slideUp();
@@ -289,15 +212,15 @@
 	});
 
 	// Input Plus & Minus Number JS
-	$('.input-counter').each(function() {
+	$('.input-counter').each(function () {
 		var spinner = jQuery(this),
-		input = spinner.find('input[type="text"]'),
-		btnUp = spinner.find('.plus-btn'),
-		btnDown = spinner.find('.minus-btn'),
-		min = input.attr('min'),
-		max = input.attr('max');
-		
-		btnUp.on('click', function() {
+			input = spinner.find('input[type="text"]'),
+			btnUp = spinner.find('.plus-btn'),
+			btnDown = spinner.find('.minus-btn'),
+			min = input.attr('min'),
+			max = input.attr('max');
+
+		btnUp.on('click', function () {
 			var oldValue = parseFloat(input.val());
 			if (oldValue >= max) {
 				var newVal = oldValue;
@@ -307,7 +230,7 @@
 			spinner.find("input").val(newVal);
 			spinner.find("input").trigger("change");
 		});
-		btnDown.on('click', function() {
+		btnDown.on('click', function () {
 			var oldValue = parseFloat(input.val());
 			if (oldValue <= min) {
 				var newVal = oldValue;
@@ -320,3 +243,29 @@
 	});
 
 })(jQuery);
+
+
+
+$(document).ready(function () {
+	const filterButtons = $(".portfolio-filters li");
+	const portfolioItems = $(".portfolio-item");
+
+	filterButtons.on("click", function () {
+		const filterValue = $(this).attr("data-filter");
+
+		// Remove 'filter-active' class from all buttons
+		filterButtons.removeClass("filter-active");
+
+		// Add 'filter-active' class to the clicked button
+		$(this).addClass("filter-active");
+
+		// Show/Hide portfolio items based on filter
+		portfolioItems.each(function () {
+			if (filterValue === "*" || $(this).hasClass(filterValue.slice(1))) {
+				$(this).show();
+			} else {
+				$(this).hide();
+			}
+		});
+	});
+});
