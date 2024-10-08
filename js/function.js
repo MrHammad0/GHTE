07377da52d1f60b050bd39d5b34380619cb2e7b8
MrHammadSandhu <mrhammadsandhu0@gmail.com>
2024-10-08 08:31:10 +0500

    adding servicesssss

--- js/function.js
+++ js/function.js
@@ -1,12 +1,13 @@
 (function ($) {
 	"use strict";
 
+
 	var $window = $(window);
 	var $body = $('body');
 
 	/* Preloader Effect */
 	$window.on('load', function () {
-		$(".preloader").fadeOut(600);
+		$(".preloader").fadeOut(900);
 	});
 
 	/* Sticky Header */
